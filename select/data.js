7efb6f447b7bf42ab766886fd8475bd2c7f503ac
m's experiments <mauricewong@me.com>
2024-10-31 16:40:35 +0800

    d

--- select/data.js
+++ select/data.js
@@ -3,13 +3,13 @@ const file = "large";
 const show = "vtk";      
 // const show = "dicom";                  
 
-const a1 = ["lesion", "red", 1.0];
-const a2 = ["skin", "green", 0.6];
+const a1 = ["thickVeins", "red", 1.0];
+const a2 = ["tumour", "green", 0.6];
 const a3 = ["dura", "blue", 1, "1"];
 const a4 = ["opening", "purple", 1, "2"];
 const a5 = ["rightSkin", "green", 0.4, "3"];
 const a6 = ["marker", 0xFFFFFF, 1, "4"];
-const models = [a1, a2, a3, a4, a5, a6];
+const models = [a1, a2,];
 
 
 const names = {
@@ -25,4 +25,3 @@ const select = {
 };
 
 export {names, models, select};
-
